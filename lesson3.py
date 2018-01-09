15a31fe4683dc7218920423cc65eed4e6e051ba0
Edwin Villanueva <edwincv0@gmail.com>
2018-01-09 15:34:35 -0800

    Start on Lesson3

--- lesson3.py
+++ lesson3.py
@@ -37,3 +37,43 @@ for t in tables: display(t.head())
 for t in tables: display(DataFrameSummary(t).summary())
 
 train, store, store_states, state_names, googletrend, weather, test = tables
+
+len(train), len(test)
+
+train.StateHoliday = train.StateHoliday!='0'
+test.StateHoliday = test.StateHoliday!='0'
+
+def join_df(left, right, left_on, right_on=None, suffix='_y'):
+    if right_on is None: right_on = left_on
+    return left.merge(right, how='left', left_on=left_on, right_on=right_on,
+                      suffixes=("", suffix))
+
+weather = join_df(weather, state_names, "file", "StateName")
+
+
+googletrend['Date'] = googletrend.week.str.split(' - ', expand=True)[0]
+googletrend['State'] = googletrend.file.str.split('_', expand=True)[2]
+googletrend.loc[googletrend.State=='NI', "State"] = 'HB,NI'
+
+add_datepart(weather, "Date", drop=False)
+add_datepart(googletrend, "Date", drop=False)
+add_datepart(train, "Date", drop=False)
+add_datepart(test, "Date", drop=False)
+
+trend_de = googletrend[googletrend.file == 'Rossmann_DE']
+
+store = join_df(store, store_states, "Store")
+len(store[store.State.isnull()])
+
+joined = join_df(train, store, "Store")
+joined_test = join_df(test, store, "Store")
+len(joined[joined.StoreType.isnull()]), len(joined_test[joined_test.StoreType.isnull()])
+
+joined = join_df(joined, googletrend, ["State", "Year", "Week"])
+joined_test = join_df(joined_test, googletrend, ["State", "Year", "Week"])
+len(joined[joined.trend.isnull()]), len(joined_test[joined_test.trend.isnull()])
+
+joined = joined.merge(trend_de, 'left', ["Year", "Week"], suffixes=('', '_DE'))
+joined_test = joined_test.merge(trend_de, 'left', ["Year", "Week"], suffixes=('', '_DE'))
+len(joined[joined.trend_DE.isnull()]), len(joined_test[joined_test.trend_DE.isnull()])
+
